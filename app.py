fafc2ba97afeaee305599ce58b4bc1804a69fa89
curcomil <sadielsalgado98@gmail.com>
2025-12-29 10:53:01 -0600

    fix routes

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ def home ():
     app.logger.info(f"Acceso a home")
     return {"mensaje": "q pedo api"}
 
+API_PREFIX = "/api"
+
 for bp, prefix in blueprints:
-    app.register_blueprint(bp, url_prefix=prefix)
+    app.register_blueprint(bp, url_prefix=API_PREFIX + prefix)
 
