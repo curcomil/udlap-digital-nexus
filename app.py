055c4464cc425a13620d49b77bd23bd80a13ecf5
curcomil <sadielsalgado98@gmail.com>
2025-12-31 15:07:47 -0600

    beta 1.0.1

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ import os
 
 load_dotenv()
 app = Flask(__name__)
-ENVIROMENT = os.getenv("ENVIROMENT")
+ENVIROMENT = os.getenv("ENVIROMENT") or "production"
+
 
 @app.route("/api", strict_slashes=False)
-def home ():
+def home():
     app.logger.info(f"Acceso a home")
     return {"mensaje": "q pedo api"}
+
 
 API_PREFIX = "/api"
 
@@ -19,4 +21,3 @@ for bp, prefix in blueprints:
 
 if __name__ == "__main__" and ENVIROMENT == "debug":
     app.run(host="127.0.0.1", port=5000, debug=True)
-
