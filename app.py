37a38bc67609c0900646ca6c5eb0e90e57eca581
curcomil <sadielsalgado98@gmail.com>
2026-01-06 13:46:06 -0600

    headers_request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from routes import blueprints
 from dotenv import load_dotenv
 import os
@@ -20,4 +20,15 @@ for bp, prefix in blueprints:
     app.register_blueprint(bp, url_prefix=API_PREFIX + prefix)
 
 if __name__ == "__main__" and ENVIROMENT == "debug":
+    
+    @app.before_request
+    def log_headers():
+        print("=== HEADERS RECIBIDOS ===")
+        print(f"Metodo: {request.method}")
+        print(f"URL: {request.url}")
+        print(f"Path: {request.path}")
+        for header, value in request.headers:
+            print(f"{header}: {value}")
+        print("========================")
+    
     app.run(host="127.0.0.1", port=5000, debug=True)
