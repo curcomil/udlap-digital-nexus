330b73817bfc4c297d8c9cfb645d044d72bba5b9
curcomil <sadielsalgado98@gmail.com>
2026-02-25 10:41:40 -0600

    index ahpsem

--- utils/index_for_collections.py
+++ utils/index_for_collections.py
@@ -1,11 +1,12 @@
 from xml.etree.ElementTree import SubElement, register_namespace
+import re
 
 # Registrar el prefijo xsi para que ElementTree no lo redeclare en cada elemento hijo
 register_namespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
 
 
 def normalize_languages(value):
-    LANG_MAP = [
+    LANG_MAP_ESP = [
         "español",
         "en español",
         "castellano",
@@ -13,15 +14,39 @@ def normalize_languages(value):
         "en espanol",
     ]
 
+    LANG_MAP_LATIN = [
+        "latín",
+        "latin",
+        "en latín",
+        "en latin",
+    ]
+
+    LANG_MAP_NAHUATL = [
+        "náhuatl",
+        "nahuatl",
+        "en náhuatl",
+        "en nahuatl",
+    ]
+
     if not value:
-        return ""
+        return []
 
-    value = value.lower()
+    # dividir por <br>, <br/> o <br />
+    idiomas = [i.strip().lower() for i in re.split(r"<br\s*/?>", value)]
 
-    if value in LANG_MAP:
-        return "spa"
-    else:
-        return "und"
+    resultado = []
+
+    for idioma in idiomas:
+        if idioma in LANG_MAP_ESP:
+            resultado.append("spa")
+        elif idioma in LANG_MAP_LATIN:
+            resultado.append("lat")
+        elif idioma in LANG_MAP_NAHUATL:
+            resultado.append("nah")
+        else:
+            resultado.append("und")  # undefined
+
+    return list(set(resultado))
 
 
 def index_4_collections(record: dict, dc: object, identifier: str):
@@ -51,16 +76,14 @@ def index_4_collections(record: dict, dc: object, identifier: str):
             if record.get("subcoleccion"):
                 elem = SubElement(dc, "dc:isPartOf")
                 elem.text = record["subcoleccion"]
-                elem.set(
-                    "xsi:type", "dcterms:isPartOf"
-                )  # ← string plano, sin expansión
+                elem.set("xsi:type", "dcterms:isPartOf")
+
+            # Lo que siempre va -------------------------
 
             if record.get("coleccion"):
                 elem = SubElement(dc, "dc:relation")
                 elem.text = record["coleccion"]
-                elem.set(
-                    "xsi:type", "dcterms:isPartOf"
-                )  # ← string plano, sin expansión
+                elem.set("xsi:type", "dcterms:isPartOf")
 
             if record.get("item_url"):
                 elem = SubElement(dc, "dc:source")
@@ -69,15 +92,66 @@ def index_4_collections(record: dict, dc: object, identifier: str):
             if record.get("portada_url"):
                 elem = SubElement(dc, "dc:source")
                 elem.text = record["portada_url"]
-                elem.set("xsi:type", "dcterms:URI")  # ← string plano, sin expansión
+                elem.set("xsi:type", "dcterms:URI")
 
             SubElement(dc, "dc:identifier").text = identifier
             SubElement(dc, "dc:type").text = "images"
 
-        # --- AQUÍ PUEDES METER OTRO CASE PARA UNA NUEVA COLECCIÓN ---
         # case 'Nombre de la Nueva Coleccion':
-        #     # Define aquí su mapeo específico...
-        #     pass
+        #
+
+        case "Archivo Histórico de la Provincia del Santo Evangelio de México":
+            # --- Archivo Histórico de la Provincia del Santo Evangelio de México ---
+            if "titulo" in metadata_interna:
+                SubElement(dc, "dc:title").text = metadata_interna["titulo"]
+
+            if "autor" in metadata_interna:
+                SubElement(dc, "dc:creator").text = metadata_interna["autor"]
+            else:
+                SubElement(dc, "dc:creator").text = "Sin autor"
+
+            if "descripcion_fisica_y_notas" in metadata_interna:
+                SubElement(dc, "dc:description").text = metadata_interna[
+                    "descripcion_fisica_y_notas"
+                ]
+
+            if "lugar" in metadata_interna:
+                SubElement(dc, "dcterms:spatial").text = metadata_interna["lugar"]
+
+            if "fecha" in metadata_interna:
+                SubElement(dc, "dc:date").text = metadata_interna["fecha"]
+
+            if "idioma" in metadata_interna:
+                idiomas_normalizados = normalize_languages(metadata_interna["idioma"])
+                for idioma in idiomas_normalizados:
+                    SubElement(dc, "dc:language").text = idioma
+
+            if "procedencia" in metadata_interna:
+                SubElement(dc, "dcterms:provenance").text = metadata_interna[
+                    "procedencia"
+                ]
+
+            if "impresor" in metadata_interna:
+                SubElement(dc, "dc:contributor").text = metadata_interna["impresor"]
+
+            # Lo que siempre va -------------------------
+
+            if record.get("coleccion"):
+                elem = SubElement(dc, "dc:relation")
+                elem.text = record["coleccion"]
+                elem.set("xsi:type", "dcterms:isPartOf")
+
+            if record.get("item_url"):
+                elem = SubElement(dc, "dc:source")
+                elem.text = record["item_url"]
+
+            if record.get("portada_url"):
+                elem = SubElement(dc, "dc:source")
+                elem.text = record["portada_url"]
+                elem.set("xsi:type", "dcterms:URI")
+
+            SubElement(dc, "dc:identifier").text = identifier
+            SubElement(dc, "dc:type").text = "archival_material_manuscript"
 
         case _:
             # --- VALORES POR DEFAULT
